7930867055359844a1266c4bf2c951ff0b53dc52
unknown <abhijitchordiya@gmail.com>
2021-12-04 21:23:24 +0400

    first commit

---
+++ src/main/java/com/springboot/app/SprintbootFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootFirstAppApplication.class, args);
+	}
+
+}
